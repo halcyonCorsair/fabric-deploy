a03100c94c079f6a15c0ca6aae362be96f666626
Elliot Pahl <elliot@catalyst.net.nz>
2012-03-07 23:27:37 +1300

    Change default commented bits in recipe, add note re: rcfile.

--- exampleconfig.py
+++ exampleconfig.py
@@ -5,17 +5,19 @@ env.site    = 'example'
 env.scm_uri = 'git+ssh://git.hosting.com/drupal/example.git'
 env.apptype = 'drupal'
 
+"""Assume stage set via rcfile, eg:
+stage=dev
+"""
+'''
 if (env.stage == 'dev'):
   env.install_tasks = [
     'build_release',
     'upload_release',
     'extract_release',
   ]
-  '''
   env.hosts = [
     'user@33.33.33.21'
   ]
-  '''
 else:
   abort('stage not setup in site recipe')
 
