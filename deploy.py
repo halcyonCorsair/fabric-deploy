3dff3ed1cee3ae3c79abd7aaf939630a98bf2c5f
Elliot Pahl <elliot@catalyst.net.nz>
2012-03-08 18:07:49 +1300

    Colour informative output.

--- deploy.py
+++ deploy.py
@@ -27,13 +27,14 @@ def load_config():
   """Load site config.
   Assume config file is called siteconfig.py and resides in the current directory.
   """
+  print green("===> Loading site recipe...")
   directory = os.getcwd()
   sys.path.append(directory)
   import siteconfig
   env.scm_build_dir = '/tmp/%s-site-%s' % (env.apptype, env.site)
 
 def tag_release(site, tag, commit, message=''):
-  print "===> Building the release..."
+  print green("===> Building the release...")
   tag = 'site-%s-' % (env.release_time)
 
   # Ensure code directory exists
@@ -53,7 +54,7 @@ def tag_release(site, tag, commit, message=''):
 # TODO: needs appropriate gitconfig, etc
 # TODO: ensure files directory and settings.php are excluded by git archive
 def build_release(site, tag):
-  print "===> Building the release..."
+  print green("===> Building the release...")
   release_archive = '%s-site-%s.tar.gz' % (env.apptype, tag)
   env.scm_build_dir = '/tmp/%s-site-%s' % (env.apptype, site)
 
@@ -71,7 +72,7 @@ def build_release(site, tag):
       local('git archive --format tar %s | gzip > ~/tmp/%s' % (release_tree, release_archive))
 
 def upload_release(site, tag):
-  print "===> Installing the site for the first time..."
+  print green("===> Uploading the release archive...")
   release_archive = '%s-site-%s.tar.gz' % (env.apptype, tag)
   with settings(warn_only=True):
     #if run("test -f /tmp/%s" % release_archive).failed:
@@ -82,7 +83,7 @@ def upload_release(site, tag):
       abort(red("Release archive doesn't exist, please run build_release again"))
 
 def extract_release(site, tag):
-  print "===> Extracting the release..."
+  print green("===> Extracting the release...")
   env.site = site
   env.tag = tag
   release_archive = '%s-site-%s.tar.gz' % (env.apptype, tag)
@@ -103,7 +104,7 @@ def create_release_settings_symlink(site, tag):
   run('ln -nfs /var/www/%s/%s/settings.php /var/www/%s/%s/releases/%s/sites/default/settings.php' % (env.apptype, site, env.apptype, site, tag))
 
 def symlink_current_release(site, tag):
-  print "===> Symlinking current release..."
+  print green("===> Symlinking current release...")
   site_symlink = '/var/www/%s/%s/current' % (env.apptype, site)
   previous_site_symlink = '/var/www/%s/%s/previous' % (env.apptype, site)
   new_previous = ''
@@ -119,26 +120,26 @@ def symlink_current_release(site, tag):
         run('ln -fns %s %s' % (new_previous, previous_site_symlink))
 
 def backup_database(site):
-  print "===> Quick and dirty database backup..."
+  print green("===> Quick and dirty database backup...")
   run('drush -r /var/www/%s/%s/current sql-dump --result-file=~/%s-`date +%Y.%m.%d-%H.%M`.sql --gzip' % (env.apptype, site, site))
 
 def site_offline(site):
-  print "===> Set site offline..."
+  print green("===> Set site offline...")
   run("drush -r /var/www/%s/%s/current -y vset maintenance_mode 1" % (env.apptype, site))
 
 def site_online(site):
-  print "===> Set site online..."
+  print green("===> Set site online...")
   run("drush -r /var/www/%s/%s/current -y vset maintenance_mode 0" % (env.apptype, site))
 
 def drush_revert_features(site):
-  print "===> Reverting site features..."
+  print green("===> Reverting site features...")
   run("drush -r /var/www/%s/%s/current fra -y" % (env.apptype, site))
 
 def drush_update_database(site):
-  print "===> Running database updates..."
+  print green("===> Running database updates...")
   run("drush -r /var/www/%s/%s/current updb" % (env.apptype, site))
 
 def drush_clear_cache_all(site):
-  print "===> Running drush cc all..."
+  print green("===> Running drush cc all...")
   run("drush -r /var/www/%s/%s/current cc all" % (env.apptype, site))
 
